f291b803da55c5745a2d2ac2239d78856cadc423
John Wilson <johnthewilson@gmail.com>
2016-07-28 04:59:38 -0700

    Holy Morgan

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var app = express();
+var morgan = require('morgan');
 
-app.use(express.logger('dev'));
+app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
